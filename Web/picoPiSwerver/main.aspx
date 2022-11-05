4a1c314f7d426f8691d40660dc9467bdd37715f1
MrMustage <noam.rosenberg.private@gmail.com>
2022-11-05 16:01:50 +0200

    added key strokes option to the web

--- Web/picoPiSwerver/main.aspx
+++ Web/picoPiSwerver/main.aspx
@@ -8,15 +8,19 @@
 </head>
 <body>
     <center>
-        <%=injection %>
+        <%=all %>
     <form id="form1" runat="server">
         <div>
+           <select name="o" id="o" size="1">key strokes
+                <option>website - "web","url"</option>
+                <option>command - just the command</option>
+                <option>key strokes - a string</option>
+            </select>
             <input type="text" id="txt" name="txt" />
             <input type="submit" id="submit" name="submit" value="submit" />
         </div>
         [system.Diagnostics.Process]::Start("msedge/chrome/firefox", <"URL">)
     </form>
-        <%=tbl %>
     </center>
 
 </body>
--- Web/picoPiSwerver/main.aspx.cs
+++ Web/picoPiSwerver/main.aspx.cs
@@ -10,21 +10,42 @@ public partial class main : System.Web.UI.Page
 {
     public string injection;
     public string tbl;
+    public string option;
+    public string all = "";
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        string db = "C:\\Users\\noamr\\Documents\\GitHub\\picoPiRCE\\Web\\picoPiSwerver\\App_Data\\Database.mdf";
+        string db = "C:\\Users\\noamr\\Documents\\GitHub\\picoPiPCE\\Web\\picoPiSwerver\\App_Data\\Database.mdf";
         string Path = "C:\\Users\\noamr\\Documents\\GitHub\\picoPiRCE\\Web\\picoPiSwerver\\";
 
         if (Request.Form["submit"] != null)
         {
             injection = Request.Form["txt"];
-            
-            MyAdoHelper.DoQuery(db,"update[Table] set str = '"+injection+"'");
+            option = Request.Form["o"];
+            if(option == "website - \"web\",\"url\"")
+            {
+                all = "[system.Diagnostics.Process]::Start(" + injection + ")"; 
+            }
+            if(option == "command - just the command")
+            {
+                all = injection;
+            }
+            if(option == "key strokes - a string")
+            {
+
+            }
+
+            if(Request.Form["txt"] == "")
+            {
+                all = null;
+            }
+
+            //MyAdoHelper.DoQuery(db,"update[Table] set str = '"+all+"'");
         }
         else
         {
-            injection = null;
-            MyAdoHelper.DoQuery(db, "update[Table] set str = 'null'");
+            all = "";
+            //MyAdoHelper.DoQuery(db, "update[Table] set str = 'null'");
         }
 
 
@@ -33,7 +54,7 @@ public partial class main : System.Web.UI.Page
             writer.WriteLine(injection);
         }
         // Read a file  
-        string readText = File.ReadAllText(Path+ "command.txtA");
+        string readText = File.ReadAllText(Path+ "command.txt");
         Console.WriteLine(readText);
 
 
